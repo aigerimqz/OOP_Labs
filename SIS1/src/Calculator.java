523dd4b7b02e7f34ba5a6618a8379b59d43eff86
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-02-28 02:31:09 +0500

    declared textfields, buttons which necessary for our Calculator

--- SIS1/src/Calculator.java
+++ SIS1/src/Calculator.java
@@ -3,11 +3,35 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class Calculator {
+public class Calculator implements ActionListener{
+	
+	JFrame frame;
+	JTextField textField;
+	JButton[] numberButtons = new JButton[10];
+	JButton[] functionButtons = new JButton[8];
+	JButton addButton, suButton, mulButton, divButton;
+	JButton decButton, equButton, delButton, clrButton;
+	JPanel panel;
+	
+	
+	
+	Font myFont = new Font("Montserrat", Font.BOLD, 30);
+	
+	
+	
+	Calculator(){
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Calculator calculator = new Calculator();
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
