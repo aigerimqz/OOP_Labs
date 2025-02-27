0d095a00e0f457c251390eb58929e138a9ab42d5
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-02-28 02:36:35 +0500

    frame settings - size, tittle, some statuses

--- SIS1/src/Calculator.java
+++ SIS1/src/Calculator.java
@@ -17,9 +17,18 @@ public class Calculator implements ActionListener{
 	
 	Font myFont = new Font("Montserrat", Font.BOLD, 30);
 	
-	
+	double num1 = 0, num2 = 0, result = 0;
+	char operator;
 	
 	Calculator(){
+		frame = new JFrame("Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420, 550);
+		frame.setLayout(null);
+		
+		
+		frame.setVisible(true);
+		
 		
 	}
 
