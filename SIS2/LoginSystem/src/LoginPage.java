63d19374ab45bf15f157c944bffbdc2b6cbb974c
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-06 15:28:50 +0500

    added all J components

--- SIS2/LoginSystem/src/LoginPage.java
+++ SIS2/LoginSystem/src/LoginPage.java
@@ -1,11 +1,32 @@
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 public class LoginPage implements ActionListener{
+	JFrame frame = new JFrame();
+	JButton loginButton = new JButton("Login");
+	JButton resetButton = new JButton("Reset");
+	JTextField userIDField = new JTextField();
+	JPasswordField userPasswordField = new JPasswordField();
+	JLabel userIDLabel = new JLabel("userID:");
+	JLabel userPasswordLabel = new JLabel("password:");
+	JLabel messageLabel = new JLabel();
 	HashMap<String, String> logininfo = new HashMap<String, String>();
 	
 	LoginPage(HashMap<String, String> loginInfoOriginal){
 		logininfo = loginInfoOriginal;
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
 		
 	}
 
