a045d2578fcfbbd5471e1c826eb0c4ad3e9e71ce
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-06 15:11:44 +0500

    added class properties

--- SIS2/LoginSystem/src/Main.java
+++ SIS2/LoginSystem/src/Main.java
@@ -1,7 +1,9 @@
 
 public class Main {
 	public static void main(String[] args) {
-		
+		IDandPasswords idandPasswords = new IDandPasswords();
+		idandPasswords
+		LoginPage loginPage = new LoginPage();
 	}
 
 }
