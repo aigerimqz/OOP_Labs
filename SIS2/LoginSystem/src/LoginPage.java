ad2f98d07f64781f6916f6ac17110c83d564a33d
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-07 08:50:34 +0500

    added welcome page settings

--- SIS2/LoginSystem/src/LoginPage.java
+++ SIS2/LoginSystem/src/LoginPage.java
@@ -71,7 +71,8 @@ public class LoginPage implements ActionListener{
 				if(logininfo.get(userID).equals(password)) {
 					messageLabel.setForeground(Color.green);
 					messageLabel.setText("Login successful");
-					WelcomePage welcomePage = new WelcomePage();
+					frame.dispose();
+					WelcomePage welcomePage = new WelcomePage(userID);
 					
 				}
 				else {
