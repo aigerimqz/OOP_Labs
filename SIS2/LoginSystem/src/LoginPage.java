48a2b9475de7699c4c62710244fed1d22ed215c9
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-07 07:31:43 +0500

    added id and password fields, then login and reset buttons

--- SIS2/LoginSystem/src/LoginPage.java
+++ SIS2/LoginSystem/src/LoginPage.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -25,9 +26,27 @@ public class LoginPage implements ActionListener{
 		userIDLabel.setBounds(50, 100, 75, 25);
 		userPasswordLabel.setBounds(50, 150, 75, 25);
 		
+		messageLabel.setBounds(125, 250, 250, 35);
+		messageLabel.setFont(new Font(null, Font.ITALIC, 25));
+		
+		userIDField.setBounds(125, 100, 200, 25);
+		userPasswordField.setBounds(125, 150, 200, 25);
+		
+		loginButton.setBounds(125, 200, 100, 25);
+		loginButton.setFocusable(false);
+		loginButton.addActionListener(this);
+		
+		resetButton.setBounds(225, 200, 100, 25);
+		resetButton.setFocusable(false);
+		resetButton.addActionListener(this);
+		
 		frame.add(userIDLabel);
 		frame.add(userPasswordLabel);
-		
+		frame.add(messageLabel);
+		frame.add(userIDField);
+		frame.add(userPasswordField);
+		frame.add(loginButton);
+		frame.add(resetButton);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(420, 420);
 		frame.setLayout(null);
