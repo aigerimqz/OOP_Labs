0fe67e950eb1b761867a0c0fd1bdbd4bc18a51b8
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-17 23:01:41 +0500

    added ForkJoinFindMax task of the 2nd task of LAB10

--- Lab10/ForkJoinFindMax/src/com/example/Main.java
+++ Lab10/ForkJoinFindMax/src/com/example/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
         int[] data = new int[1024 * 1024 * 128]; //512MB
 
-        for (int i = 0; i < data.length; i++) {
-            data[i] = ThreadLocalRandom.current().nextInt();
-        }
-
+//        for (int i = 0; i < data.length; i++) {
+//            data[i] = ThreadLocalRandom.current().nextInt();
+//        }
+//        	
 //        int max = Integer.MIN_VALUE;
 //        for (int value : data) {
 //            if (value > max) {
@@ -21,9 +21,11 @@ public class Main {
 //            }
 //        }
 //        System.out.println("Max value found:" + max);
-        
         ForkJoinPool pool = new ForkJoinPool();
-        FindMaxTask task = new FindMaxTask(data, 0, data.length-1, data.length/16);
+    	RandomArrayAction action = new RandomArrayAction(data, 0, data.length - 1, data.length / 16);
+    	pool.invoke(action);
+      
+        FindMaxTask task = new FindMaxTask(data, 0, data.length-1, data.length / 16);
         Integer result = pool.invoke(task);
         System.out.println("Max value found:" + result);
         
